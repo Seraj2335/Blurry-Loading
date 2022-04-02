6e56d82fae037986320bce8c616b16fa19c45ad1
Seraj ahmad <kseraj96@gmail.com>
2022-04-02 15:01:50 +0530

    commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
-let load = 0
+let load = 0;
 
-let loadValue = setInterval(blurring, 20);
+let integerValue = setInterval(blurring, 20);
 
 function blurring() {
   load++;
 
   if (load > 99) {
-    clearInterval(loadValue);
+    clearInterval(integerValue);
   }
 
   loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
-
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 }
